534ef805ab2f49b0c8e18226942e8bad08664888
ZaghumAbbas <zaghum@earlybirdlabs.io>
2024-11-28 22:14:03 +0500

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,34 @@
+window.addEventListener('scroll', function(){
+    let header = document.getElementById('header');
+    if(this.window.pageYOffset > 10){
+        header.classList.add('sticky');
+    }
+    else{
+        header.classList.remove('sticky');
+    }
+});
+
+
+let menu = document.getElementById("toggle-menu");
+let menuBtn = document.getElementById("call-menu");
+
+menuBtn.addEventListener('click', function (){
+    menuBtn.classList.toggle('menu-active');
+    menu.classList.toggle('active');
+
+});
+
+
+function handleMobileView() {
+    const menu = document.getElementById("toggle-menu");
+    const button = document.getElementById("interest-button");
+    if (window.innerWidth <= 1024) {
+        setTimeout(()=>{
+            menu.appendChild(button);
+            button.classList.remove("d-none");
+        }, 2000);
+
+    }
+}
+// window.addEventListener("resize", handleMobileView);
+window.addEventListener("load", handleMobileView);
